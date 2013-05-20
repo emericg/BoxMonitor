e4590fd059074edcaea71937b501cbde5fc90366
Emeric Grange <emeric.grange@gmail.com>
2013-05-20 22:30:19 +0200

    Introduce disks usage statistics using 'df' utility

--- api/controllers/DisksController.js
+++ api/controllers/DisksController.js
@@ -3,6 +3,8 @@
     -> controller
 ---------------------*/
 
+///////////////////////////////////////////////////////////////////////////////
+
 var df = require('child_process').spawn('df');
 
 var df_string = new String('loading...');
@@ -10,10 +12,10 @@ var df_lines = 0;
 
 df.stdout.on('data', function (data) {
   df_string = String(data);
-  df_lines = String(data).split("\n");
-  console.log('df stdout: ' + df_string);
-  console.log('df line: ' + df_lines);
+  //console.log('df stdout: \n' + df_lines);
 });
+
+///////////////////////////////////////////////////////////////////////////////
 
 var DisksController = {
 
@@ -22,6 +24,42 @@ var DisksController = {
 
     // Send a JSON response
     res.send(df_string, { 'Content-Type': 'text/html' }, 201);
+  },
+
+  // To trigger this action locally, visit: `http://localhost:port/disks/stats`
+  stats: function (req,res)
+  {
+    var timestamp_ms = Math.round(Date.now() / 1000);
+
+    var disks_json = [{"id": "df", "timestamp": timestamp_ms}];
+    var disks_json_content = new Object();
+
+    // FIXME Not realtime /!\
+    var disks_lines = df_string.split('\n');
+
+    // The first line only contains legend
+    disks_lines.splice(0,1);
+
+    for( var i in disks_lines ) {
+      // First, clear whitespaces
+      disks_lines[i] = disks_lines[i].replace(/ +(?= )/g, '');
+
+      if( disks_lines[i] !== '' ) {
+        disks_lines[i] = disks_lines[i].split(' ');
+
+        disks_json.push([{
+          "device": disks_lines[i][0],
+          "size": parseInt(disks_lines[i][1]),
+          "used": parseInt(disks_lines[i][2]),
+          "available": parseInt(disks_lines[i][3]),
+          "percent": parseInt(disks_lines[i][4]),
+          "mountpoint": disks_lines[i][5],
+        }]);
+      }
+    }
+
+    // Send a JSON response
+    res.json(disks_json);
   },
 
 };
